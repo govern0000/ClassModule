cb52046998dd0e8d0e5faac9763cf19368f303c2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-11-30 19:32:32 +0800

    Script

--- Script/Module/Depend.cmd
+++ Script/Module/Depend.cmd
@@ -5,6 +5,11 @@ call Script\Module\Make
 call Script\Module\Library
 popd
 
+pushd ..\MathModule
+call Script\Module\Make
+call Script\Module\Library
+popd
+
 pushd ..\Text
 call Script\Module\Make
 call Script\Module\Library
