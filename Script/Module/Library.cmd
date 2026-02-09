7c02051ea3ce8af3ead1513ffdf4f7cb2323a29e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-02-09 16:56:57 +0800

    Script

--- Script/Module/Library.cmd
+++ Script/Module/Library.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-call ..\Script\Module\Library Class.Infra-0.00.00
-call ..\Script\Module\Library Class.Binary-0.00.00
-call ..\Script\Module\Library Class.Port-0.00.00
-call ..\Script\Module\Library Class.Token-0.00.00
-call ..\Script\Module\Library Class.Node-0.00.00
-call ..\Script\Module\Library Class.Module-0.00.00
-call ..\Script\Module\Library Class.Class-0.00.00
-call ..\Script\Module\Library Class.Test-0.00.00
+call ..\Script\Module\Library Alan-Class.Infra-0.00.00
+call ..\Script\Module\Library Alan-Class.Binary-0.00.00
+call ..\Script\Module\Library Alan-Class.Port-0.00.00
+call ..\Script\Module\Library Alan-Class.Token-0.00.00
+call ..\Script\Module\Library Alan-Class.Node-0.00.00
+call ..\Script\Module\Library Alan-Class.Module-0.00.00
+call ..\Script\Module\Library Alan-Class.Class-0.00.00
+call ..\Script\Module\Library Alan-Class.Test-0.00.00
